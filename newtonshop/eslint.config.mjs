dab631476e20f5a849443f713f54ee44da8a3ec4
MisterShady <shadeytepta@gmail.com>
2024-10-29 16:03:44 +0300

    Добавлены недостающие конфиги

---
+++ newtonshop/eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
---
+++ newtonshop/vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+import eslintPlugin from "vite-plugin-eslint";
+
+export default defineConfig(async () => {
+  const tsconfigPaths = await import("vite-tsconfig-paths").then((module) => module.default);
+
+  return {
+    plugins: [
+      react(),
+      svgr(),
+      tsconfigPaths(),
+      eslintPlugin({
+        cache: false, // кэширование для более быстрой проверки
+        include: ["src/**/*.js", "src/**/*.jsx", "src/**/*.ts", "src/**/*.tsx"],
+        exclude: ["node_modules"],
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": "/src",
+      },
+    },
+    server: {
+      port: 3000, // Порт, на котором будет запущен сервер разработки
+      open: true, // Автоматически открывать браузер при запуске сервера
+      historyApiFallback: true, // позволяет Vite обрабатывать все маршруты React Router
+    },
+    build: {
+      outDir: "dist", // Директория для сборки
+      sourcemap: true, // Включить sourcemaps для production сборки
+    },
+    esbuild: {
+      // Дополнительные настройки для esbuild, если нужно
+    },
+  };
+});
