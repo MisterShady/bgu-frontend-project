f452d67c70b9a113316670605a07407302248093
MisterShady <shadeytepta@gmail.com>
2024-11-16 18:25:13 +0300

    Checkbox перенесены вверх, уведомления теперь быстрее и до конца, исправлены стили заглушек

--- newtonshop/src/components/handler/LargePlaceholder.tsx
+++ newtonshop/src/components/handler/LargePlaceholder.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Placeholder1 = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <div className="placeholder" style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
       <img src="/image/placeholder.svg" alt="Placeholder" style={{ width: "100%", height: "100%" }} />
     </div>
   );
